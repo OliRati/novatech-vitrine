8af5bdb71470f699fcd4dbe916e2b5813a15a501
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2026-02-04 12:35:25 +0100

    Update ServicesController and index template: add service titles and descriptions for improved service listing

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -11,8 +11,27 @@ final class ServicesController extends AbstractController
     #[Route('/services', name: 'app_services')]
     public function index(): Response
     {
+        $services = [
+            [
+                'title' => 'Développement web',
+                'description' => 'Création de sites et applications web modernes et performants.'
+            ],
+            [
+                'title' => 'Conseil technique',
+                'description' => 'Accompagnement dans vos choix technologiques et architectures.'
+            ],
+            [
+                'title' => 'Audit de code',
+                'description' => 'Analyse de la qualité, sécurité et maintenabilité de vos projets.'
+            ],
+            [
+                'title' => 'Maintenance applicative',
+                'description' => 'Suivi, corrections et évolutions de vos applications existantes.'
+            ]
+        ];
+
         return $this->render('services/index.html.twig', [
-            'controller_name' => 'ServicesController',
+            'services' => $services,
         ]);
     }
 }
